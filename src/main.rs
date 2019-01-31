341224a7e340a5258ce9ec8ed4b0ee94cb7965de
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-31 18:54:02 +0100

    Add calls to check import of modules

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use wavefunctions::wavefunction::Wavefunction;
-
+mod hamiltonians;
 mod particle;
 mod wavefunctions;
 
+use hamiltonians::hamiltonian;
+use wavefunctions::gaussian::Gaussian;
+use wavefunctions::wavefunction::Wavefunction;
+
+const HBAR: f64 = 1.0;
+
 fn main() {
-    let gaussian = wavefunctions::gaussian::Gaussian::new(1, 2, 3);
+    let gaussian = Gaussian::new(1, 2, 3, 2.5);
     dbg!(gaussian);
 
     let position: Vec<f64> = vec![1.0, 2.0, 3.0];
@@ -12,4 +17,6 @@ fn main() {
 
     let particle = particle::Particle::new(position, num_dimensions);
     dbg!(particle);
+
+    let ho = hamiltonians::harmonic_oscillator::HarmonicOscillator;
 }
