a2785d740c6de76c84607ecea1e705a5d87c12e4
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-31 18:54:30 +0100

    Add mass and mandatory functions

--- src/wavefunctions/gaussian.rs
+++ src/wavefunctions/gaussian.rs
@@ -5,14 +5,29 @@ pub struct Gaussian {
     num_particles: u32,
     num_dimensions: u8,
     num_parameters: u8,
+    mass: f64,
 }
 
 impl wavefunction::Wavefunction for Gaussian {
-    fn new(num_particles: u32, num_dimensions: u8, num_parameters: u8) -> Self {
+    fn new(
+        num_particles: u32,
+        num_dimensions: u8,
+        num_parameters: u8,
+        mass: f64,
+    ) -> Self {
         Gaussian {
             num_particles,
             num_dimensions,
             num_parameters,
+            mass,
         }
+    }
+
+    fn compute_laplacian(&self) -> f64 {
+        2.5
+    }
+
+    fn get_mass(&self) -> f64 {
+        self.mass
     }
 }
--- src/wavefunctions/wavefunction.rs
+++ src/wavefunctions/wavefunction.rs
@@ -1,4 +1,11 @@
 pub trait Wavefunction {
     // Constructor for a Wavefunction
-    fn new(num_particles: u32, num_dimensions: u8, num_parameters: u8) -> Self;
+    fn new(
+        num_particles: u32,
+        num_dimensions: u8,
+        num_parameters: u8,
+        mass: f64,
+    ) -> Self;
+    fn compute_laplacian(&self) -> f64;
+    fn get_mass(&self) -> f64;
 }
