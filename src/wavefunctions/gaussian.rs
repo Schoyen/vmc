6f0cb812bb5feca6669a976af62826a79a76c6b7
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-01-31 10:05:58 +0100

    Use u8 for number of dimensions and parameters
        That should be enough.

--- src/wavefunctions/gaussian.rs
+++ src/wavefunctions/gaussian.rs
@@ -3,16 +3,12 @@ use crate::wavefunctions::wavefunction;
 #[derive(Debug)]
 pub struct Gaussian {
     num_particles: u32,
-    num_dimensions: u32,
-    num_parameters: u32,
+    num_dimensions: u8,
+    num_parameters: u8,
 }
 
 impl wavefunction::Wavefunction for Gaussian {
-    fn new(
-        num_particles: u32,
-        num_dimensions: u32,
-        num_parameters: u32,
-    ) -> Self {
+    fn new(num_particles: u32, num_dimensions: u8, num_parameters: u8) -> Self {
         Gaussian {
             num_particles,
             num_dimensions,
--- src/wavefunctions/wavefunction.rs
+++ src/wavefunctions/wavefunction.rs
@@ -1,7 +1,3 @@
 pub trait Wavefunction {
-    fn new(
-        num_particles: u32,
-        num_dimensions: u32,
-        num_parameters: u32,
-    ) -> Self; // Constructor for a Wavefunction
+    fn new(num_particles: u32, num_dimensions: u8, num_parameters: u8) -> Self; // Constructor for a Wavefunction
 }
