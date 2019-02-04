eb12820b469ec944e120a8af6e6f639b1244077c
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-04 14:04:22 +0100

    Add vector of particles

--- src/system.rs
+++ src/system.rs
@@ -1,4 +1,5 @@
 use crate::hamiltonians::hamiltonian::Hamiltonian;
+use crate::particle::Particle;
 use crate::sampler::Sampler;
 use crate::wavefunctions::wavefunction::Wavefunction;
 
@@ -8,13 +9,19 @@ where
     U: Hamiltonian,
     V: Sampler,
 {
-    fn new(wavefunction: T, hamiltonian: U, sampler: V) -> Self;
+    fn new(
+        wavefunction: T,
+        hamiltonian: U,
+        sampler: V,
+        num_particles: u32,
+    ) -> Self;
 }
 
 pub struct SystemContainer<T, U, V> {
     wavefunction: T,
     hamiltonian: U,
     sampler: V,
+    particles: Vec<Particle>,
 }
 
 impl<T, U, V> System<T, U, V> for SystemContainer<T, U, V>
@@ -23,11 +30,17 @@ where
     U: Hamiltonian,
     V: Sampler,
 {
-    fn new(wavefunction: T, hamiltonian: U, sampler: V) -> Self {
+    fn new(
+        wavefunction: T,
+        hamiltonian: U,
+        sampler: V,
+        num_particles: u32,
+    ) -> Self {
         SystemContainer {
             wavefunction,
             hamiltonian,
             sampler,
+            particles: Vec::<Particle>::with_capacity(num_particles as usize),
         }
     }
 }
