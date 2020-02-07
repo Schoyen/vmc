7e2f98aaac3e5efb0df57bf936a2e90b2dfad8d5
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-02-07 12:23:36 +0100

    Call relevant functions in main

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,19 @@ use wavefunctions::Wavefunction;
 fn main() {
     let alpha = 0.4;
     let step_length = 1.0;
+    let spread = 2.0;
     let num_parameters: usize = 1;
     let num_dimensions: usize = 1;
     let num_particles: usize = 10;
+    let num_metropolis_steps: usize = 10000;
 
     let particles = Particles::new(num_particles, num_dimensions);
     let gaussian = Gaussian::new(alpha);
 
     let ho = HarmonicOscillator;
 
-    let system =
-        System::new(gaussian, ho, particles, Sampler::new(), step_length);
+    let mut system = System::new(gaussian, ho, particles, step_length);
+    system.initialize_walkers(spread);
+
+    let sampler = system.run_metropolis_steps(num_metropolis_steps);
 }
