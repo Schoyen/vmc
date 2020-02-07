cf0f777bf2a1d3606c9f5ef17105fac309bdb61e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-02-07 12:23:19 +0100

    Start implementing metropolis algorithm

--- src/system.rs
+++ src/system.rs
@@ -1,3 +1,5 @@
+use rand::{prelude::thread_rng, Rng};
+
 use crate::hamiltonians::Hamiltonian;
 use crate::particle::{Particle, Particles};
 use crate::sampler::Sampler;
@@ -8,7 +10,6 @@ pub struct System<T, U> {
     wavefunction: T,
     hamiltonian: U,
     particles: Particles,
-    sampler: Sampler,
     step_length: f64,
 }
 
@@ -21,14 +22,12 @@ where
         wavefunction: T,
         hamiltonian: U,
         particles: Particles,
-        sampler: Sampler,
         step_length: f64,
     ) -> Self {
         System {
             wavefunction,
             hamiltonian,
             particles,
-            sampler,
             step_length,
         }
     }
@@ -37,7 +36,20 @@ where
         self.particles.distribute_particles(spread);
     }
 
-    pub fn run_metropolis_steps(&self, num_steps: u64) {
-        self.sampler.sample(&self);
+    pub fn run_metropolis_steps(&self, num_steps: usize) -> Sampler {
+        let sampler = Sampler::new(num_steps);
+
+        sampler.sample(&self, true);
+
+        for step in 0..num_steps {
+            sampler.sample(&self, self.run_metropolis_step());
+        }
+
+        sampler
+    }
+
+    fn run_metropolis_step(&self) -> bool {
+        let old_wfn = self.wavefunction.evaluate(&self.particles);
+        true
     }
 }
