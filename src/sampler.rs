e0ce2816c68104beec2fba0973a723eb00a4fdd7
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-02-07 12:22:56 +0100

    Store array of local energies

--- src/sampler.rs
+++ src/sampler.rs
@@ -4,15 +4,21 @@ use crate::wavefunctions::Wavefunction;
 
 #[derive(Debug)]
 pub struct Sampler {
+    num_metropolis_steps: usize,
     foo: u32,
+    local_energies: Vec<f64>,
 }
 
 impl Sampler {
-    pub fn new() -> Self {
-        Sampler { foo: 0 }
+    pub fn new(num_metropolis_steps: usize) -> Self {
+        Sampler {
+            num_metropolis_steps,
+            foo: 0,
+            local_energies: vec![0.0; num_metropolis_steps],
+        }
     }
 
-    pub fn sample<T, U>(&self, system: &System<T, U>)
+    pub fn sample<T, U>(&self, system: &System<T, U>, accepted: bool)
     where
         T: Wavefunction,
         U: Hamiltonian,
