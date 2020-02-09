38152647bf2a2c34dabb6e52d96eb54c9eba1529
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-02-09 15:23:28 +0100

    Store mass and frequency in Hamiltonian

--- src/hamiltonians/harmonic_oscillator.rs
+++ src/hamiltonians/harmonic_oscillator.rs
@@ -2,7 +2,16 @@ use super::Hamiltonian;
 use crate::particle::Particles;
 use crate::wavefunctions::Wavefunction;
 
-pub struct HarmonicOscillator;
+pub struct HarmonicOscillator {
+    mass: f64,
+    omega: f64,
+}
+
+impl HarmonicOscillator {
+    pub fn new(mass: f64, omega: f64) -> Self {
+        HarmonicOscillator { mass, omega }
+    }
+}
 
 impl Hamiltonian for HarmonicOscillator {
     fn compute_kinetic_energy<T>(
@@ -13,17 +22,20 @@ impl Hamiltonian for HarmonicOscillator {
     where
         T: Wavefunction,
     {
-        -0.5 * wavefunction.compute_laplacian(particles)
+        -wavefunction.compute_laplacian(particles) / (2.0 * self.mass)
     }
 
     fn compute_potential_energy<T>(
         &self,
-        wavefunction: &T,
+        _wavefunction: &T,
         particles: &Particles,
     ) -> f64
     where
         T: Wavefunction,
     {
-        2.5
+        0.5 * self.mass
+            * self.omega
+            * self.omega
+            * particles.compute_pos_squared_sum()
     }
 }
