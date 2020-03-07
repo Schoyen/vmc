2c5e75b0c5223b7b911181e5fa5f945e84097d6c
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-03-07 15:59:19 +0100

    Add run-example using importance sampling

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod wavefunctions;
 
 use hamiltonians::HarmonicOscillator;
 use particle::Particles;
-use solvers::MetropolisAlgorithm;
+use solvers::{ImportanceSampling, MetropolisAlgorithm};
 use system::System;
 use wavefunctions::Gaussian;
 
@@ -21,6 +21,9 @@ fn main() {
     let num_particles: usize = 10;
     let num_metropolis_steps: usize = 1_000_00;
 
+    let imp_step_length = 0.1;
+    let diffusion_coefficient = 0.5;
+
     let particles = Particles::new(num_particles, num_dimensions);
     let gaussian = Gaussian::new(alpha);
 
@@ -31,5 +34,18 @@ fn main() {
 
     let sampler =
         system.run_metropolis_steps(&MetropolisAlgorithm, num_metropolis_steps);
+    sampler.output_statistics();
+
+    let particles = Particles::new(num_particles, num_dimensions);
+    let gaussian = Gaussian::new(alpha);
+
+    let ho = HarmonicOscillator::new(mass, omega);
+
+    let mut system = System::new(gaussian, ho, particles, imp_step_length);
+    system.initialize_walkers(spread);
+
+    let solver = ImportanceSampling::new(diffusion_coefficient);
+
+    let sampler = system.run_metropolis_steps(&solver, num_metropolis_steps);
     sampler.output_statistics();
 }
